cee072627da4ca95972b5c972ab5df036284df97
akhaniShaik <shaikakhani84@gmail.com>
2020-11-05 15:38:33 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Square from "./Square";
+import React, { useState, useEffect } from "react";
+
+const intialState = ["", "", "", "", "", "", "", "", ""];
 
 function App() {
+  const [gameState, updateGameState] = useState(intialState);
+  const [isXChance, updateIsXChance] = useState(false);
+  const onSquareClick = (index) => {
+    let strings = Array.from(gameState);
+    strings[index] = isXChance ? "X" : "0";
+    updateGameState(strings);
+    updateIsXChance(!isXChance);
+  };
+  useEffect(() => {
+    const winner = checkWinner();
+    if (winner) {
+      alert(`Ta da ! ${winner} has won the game!`);
+      updateGameState(intialState);
+    }
+  }, [gameState]);
+  const checkWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        gameState[a] &&
+        gameState[a] === gameState[b] &&
+        gameState[a] === gameState[c]
+      ) {
+        return gameState[a];
+      }
+    }
+    return null;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-header">
+      <p className="heading-text">React Tic Tac Toe- 2020 !</p>
+      <div className="row jc-center">
+        <Square
+          className="b-bottom-right"
+          state={gameState[0]}
+          onClick={() => onSquareClick(0)}
+        />
+        <Square
+          className="b-bottom-right"
+          state={gameState[1]}
+          onClick={() => onSquareClick(1)}
+        />
+        <Square
+          className="b-bottom "
+          state={gameState[2]}
+          onClick={() => onSquareClick(2)}
+        />
+      </div>
+      <div className="row jc-center">
+        <Square
+          className="b-bottom-right"
+          state={gameState[3]}
+          onClick={() => onSquareClick(3)}
+        />
+        <Square
+          className="b-bottom-right"
+          state={gameState[4]}
+          onClick={() => onSquareClick(4)}
+        />
+        <Square
+          className="b-bottom"
+          state={gameState[5]}
+          onClick={() => onSquareClick(5)}
+        />
+      </div>
+      <div className="row jc-center">
+        <Square
+          className="b-right"
+          state={gameState[6]}
+          onClick={() => onSquareClick(6)}
+        />
+        <Square
+          className="b-right"
+          state={gameState[7]}
+          onClick={() => onSquareClick(7)}
+        />
+        <Square state={gameState[8]} onClick={() => onSquareClick(8)} />
+      </div>
+      <button
+        className="clear-button"
+        onClick={() => updateGameState(intialState)}
+      >
+        Clear Game
+      </button>
+      <p className="fc-aqua fw-600">AKhani@Tencent Game 2020.</p>
     </div>
   );
 }
